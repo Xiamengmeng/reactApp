cbb1cc5c1d6ed4853e1315b5470aec0975ea355a
Xiamengmeng <940503239@qq.com>
2020-07-02 17:10:06 +0800

    use function method

--- src/components/Namecard.js
+++ src/components/Namecard.js
@@ -1,24 +1,47 @@
 import React from 'react'
-class Namecard extends React.Component{
-    //  
-    render(){
-        const {name,number,kind,tags}=this.props
-        return(
-            <div alert alert-success>
-                <h4>{name}</h4>
-                <ul>
-                    <li>电话：{number}</li>
-                    <li>{kind?'人类':'外星生物'}</li>
-                </ul>
-                {tags.map((item,index)=>( <span key={index}>{item}</span>
-)
+// class Namecard extends React.Component{
+//     //  
+//     render(){
+//         const {name,number,kind,tags}=this.props
+//         return(
+//             <div alert alert-success>
+//                 <h4>{name}</h4>
+//                 <ul>
+//                     <li>电话：{number}</li>
+//                     <li>{kind?'人类':'外星生物'}</li>
+//                 </ul>
+//                 {tags.map((item,index)=>( <span key={index}>{item}</span>
+// )
                    
-                )
-                }
+//                 )
+//                 }
 
                 
-            </div>
-        )
-    }
+//             </div>
+//         )
+//     }
+// }
+
+
+// 改写成函数模式
+const Namecard=(props)=>{
+    const {name,number,kind,tags}=props
+    return(
+        <div alert alert-success>
+            <h4>{name}</h4>
+            <ul>
+                <li>电话：{number}</li>
+                <li>{kind?'人类':'外星生物'}</li>
+            </ul>
+            {tags.map((item,index)=>( <span key={index}>{item}</span>
+            )
+            
+            )
+            }
+
+            
+        </div>
+    )    
 }
+
 export default Namecard
