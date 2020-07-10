5388922b8d1a2f623bdacc377f37c835969a00be
Xiamengmeng <940503239@qq.com>
2020-07-10 11:16:57 +0800

    unControll part

--- src/components/Formcontent.js
+++ src/components/Formcontent.js
@@ -4,23 +4,30 @@ class Formcontent extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            value:'',
+            // value:'',
             list:['uu','oo','pp']
         }
+        this.input=React.createRef();
+        // this.sub=React.createRef();
     }
     changeHandle(e){
-        console.log(e.target.value)
-        this.setState({
-            value:e.target.value
-        })
+        // console.log(e.target.value)
+        // this.setState({
+        //     value:e.target.value
+        // })
+        console.log(this.input)
     }
     clickHandle(e){
-        // e.preventDefault()
-        console.log('click')
+        e.preventDefault()
+        // console.log('click')
         // alert(this.state.value)
-        this.state.list.push(this.state.value)
+        this.state.list.push(this.input.current.value)
         console.log(this.state.list)
         this.setState({list:this.state.list})
+
+        // this.sub.push(this.input.current.value)
+        // console.log(this.sub)
+
     }
     submitHandle(){
         console.log('submit')
@@ -31,12 +38,11 @@ class Formcontent extends React.Component{
             <div> 
                 <form onSubmit={this.submitHandle.bind(this)}>
                     <label>留言内容：</label>
-                    <input type='text' onChange={this.changeHandle.bind(this)}/>
+                    <input type='text' defaultValue='' ref={this.input}  onChange={this.changeHandle.bind(this) }/>
                     <input type='submit' onClick={this.clickHandle.bind(this)} placeholder='留言'/>
                 </form>
                 <ul>
                     {this.state.list.map((item,index)=>(<li key={index}>{item}</li>))}
-                 {/* <li>{this.state.list.map((item,index)=>{item})}</li> */}
 
                 </ul>
 
