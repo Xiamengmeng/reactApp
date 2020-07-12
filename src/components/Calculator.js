21c0dbb476387724e6f48867de1222379fd4ba78
Xiamengmeng <940503239@qq.com>
2020-07-12 18:34:36 +0800

    add something

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,7 +19,27 @@ class Calculator extends React.Component{
       handleFahrenheitChange(temperature) {
         this.setState({scale: 'f', temperature});
       }
+    //   ctof(c){
+    //         return  c*9/5+32
+    //     }
+    //   ftoc(f){
+    //         return (f-32)*5/9
+
+    //     }
+    //     tryConvert(){
+    //      const input = parseFloat(this.props.temperature)
+    //      if(Number.isNaN(input)){
+    //          return ''
+    //      }
+    //      const output = convert(input);
+    //      const rounded=Math.round(output*1000)/1000;
+    //      return rounded.toString()
+    //  }
     render(){
+        // const scale = this.state.scale;
+        // const temperature = this.state.temperature;
+        // const celsius = scale === 'f' ? tryConvert(temperature, ftoc) : temperature;
+        // const fahrenheit = scale === 'c' ? tryConvert(temperature, ctof) : temperature;
         return(
             <div>
                 {/* <input value={this.state.temperture} onChange={this.changeHandle.bind(this)}/> */}
